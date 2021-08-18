3c5855097f0114e60f546119ef3811ddbe52f493
How old are you ?? <54581408+hieu2k1@users.noreply.github.com>
2021-08-18 17:45:53 +0700

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using DemoProjectSem3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Web.Mvc;
+using DemoProjectSem3.Context;
+
+namespace DemoProjectSem3.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private DemoContext obj = new DemoContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Models/Sanpham.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoProjectSem3.Models
+{
+    public class Sanpham : Controller
+    {
+        // GET: Sanpham
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
